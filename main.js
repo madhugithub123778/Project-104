788a029dcba4aeb97ab13f7253fbf76516e78949
madhugithub123778 <74599077+madhugithub123778@users.noreply.github.com>
2021-05-13 18:36:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ Webcam.set({
     
     cam = document.getElementById("camera");
     Webcam.attach(cam);
+
+function takePicture(){
+Webcam.snap(
+function(data_uri){
+document.getElementById("result").innerHTML= '<img id="my_image"src="'+data_uri+'"/>';
+});
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/NHqki6Xdk/model.json",modelLoaded);
+
+function modelLoaded(){
+console.log("Model is loaded");}
