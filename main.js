4f24c4986a2b3cc234cbba72b940a7f9fcbda42c
madhugithub123778 <74599077+madhugithub123778@users.noreply.github.com>
2021-05-16 17:01:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,3 +20,20 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 
 function modelLoaded(){
 console.log("Model is loaded");}
+
+function identify(){
+img = document.getElementById("my_image");
+classifier.classify(img, gotResult);}
+
+function gotResult(error, results){
+if(error){
+console.log(error);}
+
+else{
+console.log(results);
+document.getElementById("person_name_result").innerHTML= results[0].label;
+document.getElementById("accuracy_percent_result").innerHTML= results[0].confidence.toFixed(3);}
+
+
+
+}
